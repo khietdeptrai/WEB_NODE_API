92af116646d5ba9e497ca0faf044e16b129b13dd
tangquochung <19521583@gm.uit.edu.vn>
2022-11-23 18:12:17 +0700

    Hung - up

--- routes/auth.js
+++ routes/auth.js
@@ -1,12 +1,18 @@
 const router = require("express").Router();
 const User = require("../models/User"); 
+const CryptoJS = require("crypto-js");
+
 
 //REGISTER
 router.post("/register", async (req, res) => {
     const newUser = new User({
         username: req.body.username,
         email: req.body.email,
-        password: req.body.password,
+        //password: req.body.password, //normal password req
+        password: CryptoJS.AES.encrypt(
+            req.body.password, 
+            process.env.PASS_SEC
+        ).toString(), //encrypt password
     });
     
     try {
