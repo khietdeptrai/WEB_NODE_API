53abc82728f2e210e60302bfa8ff880b596f6419
tangquochung <19521583@gm.uit.edu.vn>
2022-12-11 22:05:36 +0700

    Up

---
+++ app/config/auth.config.js
@@ -0,0 +1,3 @@
+module.exports = {
+    secret: "hieunt2001"
+  };
---
+++ app/config/cloudinary.js
@@ -0,0 +1,9 @@
+const cloudinary = require('cloudinary').v2;
+
+cloudinary.config({
+  cloud_name: 'di8lhw5co',
+  api_key:'179384832474541',
+  api_secret: 'FhkJlIQy_GMid1RaOIx-5JqjBqE',
+});
+
+module.exports = cloudinary;
